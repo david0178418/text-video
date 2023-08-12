e5ceccc41f157350591ab7431fb1db2d569b76fe
David Granado <davidjgranado@gmail.com>
2023-08-12 18:35:58 -0500

    Render to canvas

--- src/components/foo.tsx
+++ src/components/foo.tsx
@@ -1,15 +1,70 @@
-import { Center } from '@styled-system/jsx';
+'use client';
 
-export default
-function Foo() {
+import { Center } from '@styled-system/jsx';
+import { domToCanvas } from 'modern-screenshot';
+import RevealText from './reveal-text';
+import { css } from '@styled-system/css';
+import { useInterval } from 'usehooks-ts';
+import {
+	useEffect,
+	useRef,
+	useState,
+} from 'react';
+
+export default function Foo() {
+	const canvasRef = useRef<HTMLCanvasElement>(null);
+	const sourceRef = useRef<HTMLDivElement>(null);
+	const [isRecording, setIsRecording] = useState(true);
+	const delay = 50;
+	const width = 512;
+	const height = 288;
+
+	useEffect(() => {
+		if(!canvasRef.current) return;
+
+	}, []);
+
+	useInterval(async () => {
+		if(!canvasRef.current || !sourceRef.current) return;
+
+		console.log('recording');
+
+		// TODO: Is here a more efficient way to do this?
+		const canvas = await domToCanvas(sourceRef.current);
+		canvasRef.current.getContext('2d')?.drawImage(canvas, 0, 0);
+	}, isRecording ? delay : null);
+
 	return (
 		<Center flexDir="column">
-			<div>
-				Foo
+			<div
+				ref={sourceRef}
+				className={css({
+					height,
+					width,
+					backgroundColor: 'lightgray',
+					fontSize: '16px',
+					textAlign: 'center',
+					display: 'flex',
+					flexDirection: 'column',
+					justifyContent: 'center',
+					padding: '20px',
+				})}
+			>
+				<RevealText
+					delay={delay}
+					onComplete={() => setIsRecording(false)}
+				>
+					foofoofoofoofoofoofoofoo foofoofoofoofoofoofoofoo
+					foofoofoofoofoofoofoofoo foofoofoofoofoofoofoofoo
+					foofoofoofoofoofoofoofoo foofoofoofoofoofoofoofoo
+				</RevealText>
 			</div>
-			<div>
-				Bar
-			</div>
+			<canvas
+				ref={canvasRef}
+				height={height}
+				width={width}
+				className={css({ border: '1px solid black' })}
+			/>
 		</Center>
 	);
 }
--- src/components/reveal-text.tsx
+++ src/components/reveal-text.tsx
@@ -1,20 +1,30 @@
 'use client';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useInterval } from 'usehooks-ts';
 
 interface Props {
 	children: string;
+	delay?: number;
+	onComplete?: () => void;
 }
 
 export default
 function RevealText(props: Props) {
-	const { children } = props;
+	const {
+		children,
+		delay = 100,
+		onComplete,
+	} = props;
 	const [revealedCharacterCount, setRevealedCharacterCount] = useState(0);
 	const isPlaying = revealedCharacterCount < children.length;
 
+	useEffect(() => {
+		if(!isPlaying) onComplete?.();
+	}, [isPlaying]);
+
 	useInterval(
 		() => setRevealedCharacterCount(revealedCharacterCount + 1),
-		isPlaying ? 100 : null,
+		isPlaying ? delay : null,
 	);
 
 	return (
